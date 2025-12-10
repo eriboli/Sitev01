a50c189b68ebc90698a9b4fdf7c94ab3fa56cfd4
Eduardo Riboli <eriboli@gmail.com>
2025-12-10 00:28:45 -0300

    Deploy Sitev1

---
+++ footer.js
@@ -0,0 +1,20 @@
+document.getElementById('common-footer').innerHTML = `
+    <footer class="footer">
+        <div class="light-footer">
+            <a href="/">
+                <img src="./imgs/logo_ger.png">
+            </a>
+            <nav>
+                <ul class="menu">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Ramos</a></li>
+                    <li><a href="/">Contato</a></li>
+                    <li><a href="/">Menu</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="dark-footer">
+            <p>Copyright 2001</p>
+        </div>
+    </footer>
+`;
