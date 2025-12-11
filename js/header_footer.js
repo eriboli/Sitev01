e13a7933e182de37cc51d544b55d8c56f107e852
Eduardo Riboli <eriboli@gmail.com>
2025-12-10 22:12:41 -0300

    Navtop responsivo

--- js/header_footer.js
+++ js/header_footer.js
@@ -1,24 +1,5 @@
 document.getElementById('common-header').innerHTML = `
-    <header class="header">
-        <a class="logo-link" href="index.html">
-            <div class="logo">
-                <img src="./imgs/logo_ger_color.png">
-                <div class="logo-text">
-                    <p>Grupo Escoteiro</p>
-                    <p>Rastreadores</p>
-                </div>
-            </div>
-        </a>
-        <div class="menu">
-            <nav class="navtop">
-                <a class="show-on-index button-style" href="cadastro.html"><i class="fa-solid fa-feather-pointed"></i>Inscreva-se</a>
-                <a href="jovens.html"><i class="fa-solid fa-child-reaching" aria-hidden="true"></i>Jovens</a>
-                <a href="acoes-sociais.html"><i class="fa-solid fa-hand-holding-heart"></i>Ações Sociais</a>
-                <a href="doe.html"><i class="fa-solid fa-circle-dollar-to-slot"></i>Doe</a>
-                <a href="menu.html"><i class="fa-solid fa-bars"></i>Menu</a> 
-            </nav>
-        </div>
-    </header>
+    
 `;
 
 
