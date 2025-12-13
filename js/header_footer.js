3c18d94818b43695035bdb508d6949a063bf7d3d
Eduardo Riboli <eriboli@gmail.com>
2025-12-13 18:45:17 -0300

    Ajustes de responsividade

--- js/header_footer.js
+++ js/header_footer.js
@@ -1,5 +1,6 @@
 document.getElementById('common-header').innerHTML = `
     <header class="header">
+        <div class="top">
             <a class="logo-link" href="index.html">
                 <div class="logo">
                     <img src="./imgs/logo_ger_color.png">
@@ -9,53 +10,55 @@ document.getElementById('common-header').innerHTML = `
                     </div>
                 </div>
             </a>
-            <div class="menu">
-                <nav class="navtop">
-                    <a class="show-on-index button-style" href="cadastro.html"><i class="fa-solid fa-feather-pointed"></i><span class="nav-text">Inscreva-se</span></a>
-                    <a href="jovens.html"><i class="fa-solid fa-child-reaching" aria-hidden="true"></i><span class="nav-text">Jovens</span></a>
-                    <a href="acoes-sociais.html"><i class="fa-solid fa-hand-holding-heart"></i><span class="nav-text">Ações Sociais</span></a>
-                    <a href="doe.html"><i class="fa-solid fa-circle-dollar-to-slot"></i><span class="nav-text">Doe</span></a>
-                    <a href="menu.html"><i class="fa-solid fa-bars"></i><span class="nav-text">Menu</span></a> 
-                </nav>
-            </div>
+            <nav class="navtop">
+                <a class="show-on-index button-style" href="cadastro.html"><i class="fa-solid fa-feather-pointed"></i><span class="nav-text">Inscreva-se</span></a>
+                <a href="jovens.html"><i class="fa-solid fa-child-reaching" aria-hidden="true"></i><span class="nav-text">Jovens</span></a>
+                <a href="acoes-sociais.html"><i class="fa-solid fa-hand-holding-heart"></i><span class="nav-text">Ações Sociais</span></a>
+                <a href="doe.html"><i class="fa-solid fa-circle-dollar-to-slot"></i><span class="nav-text">Doe</span></a>
+                <a href="menu.html"><i class="fa-solid fa-bars"></i><span class="nav-text">Menu</span></a> 
+            </nav>
+        </div>
     </header>
 `;
 
 
 document.getElementById('common-footer').innerHTML = `
-    <footer class="footer">
-        <div class="top-footer">
-            <div class="logo-footer">
-                <img src="./imgs/logo_ger_white.png">
-                <div class="logo-text">
-                    <p>Grupo Escoteiro</p>
-                    <p>Rastreadores</p>
-                </div>
-            </div>
-            <div class="data-footer">
-                <div class="data-footer-column">
-                    <h3>Links</h3>
-                    <div>
-                        <a href="http://paxtu.escoteiros.org.br/">Paxtu</a>
-                        <a href="http://mail.rastreadores293.com.br/">Webmail</a>
+    <footer class="footer">   
+        <div class="top-footer-base">
+            <div class="top-footer">
+                <div class="logo-footer">
+                    <img src="./imgs/logo_ger_white.png">
+                    <div class="logo-text">
+                        <p>Grupo Escoteiro</p>
+                        <p>Rastreadores</p>
                     </div>
                 </div>
-                <div class="data-footer-column">
-                    <h3>Legal</h3>
-                    <div>
-                        <a href="https://rastreadores293.com.br/politica-de-privacidade/">Política de privacidade</a>
-                        <a href="#">Termos de uso</a>
+                <div class="data-footer">
+                    <div class="data-footer-column">
+                        <h3>Links</h3>
+                        <div>
+                            <a href="http://paxtu.escoteiros.org.br/">Paxtu</a>
+                            <a href="http://mail.rastreadores293.com.br/">Webmail</a>
+                        </div>
                     </div>
+                    <div class="data-footer-column">
+                        <h3>Legal</h3>
+                        <div>
+                            <a href="https://rastreadores293.com.br/politica-de-privacidade/">Política de privacidade</a>
+                            <a href="#">Termos de uso</a>
+                        </div>
+                    </div>
+                    <div class="data-footer-column">
+                        <h3>Info</h3>
+                        <div>
+                            <p>Sábados, das 14:00 às 17:00</p>
+                            <a href="https://goo.gl/maps/estRwABPnUdxwmYo9">Rua Gongalves Dias, 590 - Portão/RS</a>
+                        </div>
+                    </div> 
                 </div>
-                <div class="data-footer-column">
-                    <h3>Info</h3>
-                    <div>
-                        <p>Sábados, das 14:00 às 17:00</p>
-                        <a href="https://goo.gl/maps/estRwABPnUdxwmYo9">Rua Gongalves Dias, 590 - Portão/RS</a>
-                    </div>
-                </div> 
             </div>
         </div>
+        
         <div class="bottom-footer">
             <div>
                 <p>Copyright © 2001-2025 Grupo Escoteiro Rastreadores</p>
