65f70188c31679cd50946174197b9d653c6fe0ad
Eduardo Riboli <eriboli@gmail.com>
2025-12-16 21:28:36 -0300

    Contact form fixed

--- js/postdata.js
+++ js/postdata.js
@@ -1,40 +1,17 @@
-// const gscript = 'https://script.google.com/macros/s/AKfycbwExY32Z_OenpIiwCXUhdPZ1C9MduDlpGJ4wgqhHsWGTCt_jXIHRvFsAdJ0By1_LsMXvw/exec';
+const gscript = 'https://script.google.com/macros/s/AKfycbw_iSSHJ8qhxx_wPRdu_Gh8n8j3OTIbUXiPhdbMrCo6UNmQBzCDfeJDnSu6_44aL6OaUA/exec';
 
-// const form_data = document.forms['formulario-contato'];
+const form_data = document.forms['formulario-contato'];
 
-// form_data.addEventListener('submit', function (e) {
-//     e.preventDefault();
-
-//     fetch(gscript, { method: 'POST', body: new FormData(form_data) })
-//         .then(response => {
-//             // Se os dados forem gravados corretamente, será enviada uma mensagem de sucesso
-//             alert('Dados enviados com sucesso!', response);
-//             form_data.reset(); 
-//         })
-//         .catch(error => {
-//             // Se houver erro no envio, a menssagem abaixo será exibida
-//             console.error('Erro no envio dos dados, meu!!', error);
-//         });
-// });
-
-
-
-
-// New code
-
-let gScript = 'https://script.google.com/macros/s/AKfycbwExY32Z_OenpIiwCXUhdPZ1C9MduDlpGJ4wgqhHsWGTCt_jXIHRvFsAdJ0By1_LsMXvw/exec';
-
-let form = document.querySelector('form');
-
-form.addEventListener('submit', function (e) {
+form_data.addEventListener('submit', function (e) {
     e.preventDefault();
-    let data = new FormData(form);
-    fetch(gScript, {
-        method: 'POST',
-        body: data
-    })
-        .then(res => res.test())
-        .then(data => console.log(data))
-        .catch(err => console.log(err));
-        form.reset();
+    fetch(gscript, { method: 'POST', body: new FormData(form_data) })
+        .then(response => {
+            // Se os dados forem gravados corretamente, será enviada uma mensagem de sucesso
+            alert('Dados enviados com sucesso!', response);
+            form_data.reset(); 
+        })
+        .catch(error => {
+            // Se houver erro no envio, a menssagem abaixo será exibida
+            console.error('Erro no envio dos dados, meu!!', error);
+        });
 });
